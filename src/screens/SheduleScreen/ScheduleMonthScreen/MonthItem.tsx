9c2da662a889a1eeb41515bf091234c40b1b4a36
Serhii <sserhiivalko@gmail.com>
2023-05-26 18:21:08 +0200

    fix month

--- src/screens/SheduleScreen/ScheduleMonthScreen/MonthItem.tsx
+++ src/screens/SheduleScreen/ScheduleMonthScreen/MonthItem.tsx
@@ -16,33 +16,33 @@ interface IMonthItem {
 export const MonthItem: FC<IMonthItem> = memo(({ sesion, day, isCurrentMonth, item }) => {
     const hasLesson = sesion && isCurrentMonth
     return (
-        <TouchableOpacity onPress={() => console.log(item, 'item info')}>
-            <View style={styles.itemContainer}>
-                <LinearGradient
-                    colors={
-                        hasLesson ? ['#EAAFC8', '#654EA3'] : ['transparent', 'transparent']
-                    }
-                    start={{ x: 0, y: 0 }}
-                    end={{ x: 0, y: 1 }}
-                    style={{
-                        width: '100%',
-                        height: '100%',
-                        borderRadius: 9,
-                        alignItems: 'center',
-                    }}>
-                    <Text
-                        style={[
-                            styles.monthItemText,
-                            hasLesson ? styles.monthItemActiveText : null,
-                            !isCurrentMonth && { opacity: 0.5 }
-                        ]}>
-                        {day}
-                    </Text>
-                    {hasLesson ? (
-                        <Text style={styles.numberOfClasses}>{`${sesion} Sessions`}</Text>
-                    ) : null}
-                </LinearGradient>
-            </View>
+        <TouchableOpacity onPress={() => console.log(item, 'item info')} style={styles.itemContainer}>
+            {/* <View > */}
+            <LinearGradient
+                colors={
+                    hasLesson ? ['#EAAFC8', '#654EA3'] : ['transparent', 'transparent']
+                }
+                start={{ x: 0, y: 0 }}
+                end={{ x: 0, y: 1 }}
+                style={{
+                    width: '100%',
+                    height: '100%',
+                    borderRadius: 9,
+                    alignItems: 'center',
+                }}>
+                <Text
+                    style={[
+                        styles.monthItemText,
+                        hasLesson ? styles.monthItemActiveText : null,
+                        !isCurrentMonth && { opacity: 0.5 }
+                    ]}>
+                    {day}
+                </Text>
+                {hasLesson ? (
+                    <Text style={styles.numberOfClasses}>{`${sesion} Sessions`}</Text>
+                ) : null}
+            </LinearGradient>
+            {/* </View> */}
         </TouchableOpacity>
     );
 });

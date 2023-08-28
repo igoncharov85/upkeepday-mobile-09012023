90bcd9f268f4cc66ad4df0ca18616130486c4d8b
Igor Goncharov <ihar@hancharou.com>
2023-08-28 15:55:16 -0400

    Configuration for App Store Upload

--- src/common/constants/environment/live.env.ts
+++ src/common/constants/environment/live.env.ts
@@ -1,4 +1,12 @@
 export const environment = {
+    // Localhost
+    // apiUrl: 'http://127.0.0.1:5000',
+    // dev
+    //apiUrl: 'https://api.upkeepday.com:8443',
+    // qa
     apiUrl: 'https://api.upkeepday.com',
-    //apiUrl: 'http://127.0.0.1:5000',
+    // staging
+    // apiUrl: 'https://api.upkeepday.com:9999',
+    // production
+    //apiUrl: 'https://tip.upkeepday.com'
 }

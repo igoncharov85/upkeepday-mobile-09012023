e00c213d212095d3987542645542821f1d73d24b
NazarenkoAnna2021 <111137808+Olegmaksimmenko@users.noreply.github.com>
2023-09-01 12:17:48 +0300

    fix SheduleHeader

--- src/screens/SheduleScreen/components/SheduleHeader/index.tsx
+++ src/screens/SheduleScreen/components/SheduleHeader/index.tsx
@@ -1,25 +1,44 @@
-import React, { FC, memo, useMemo } from 'react';
-import { Text, View } from 'react-native';
+import React, { FC, memo, useMemo, useState } from 'react';
+import { Text, TouchableOpacity, View } from 'react-native';
 import SearchIcon from '../../../../../assets/svg/SearchIcon';
 import { AccountSelector } from './components/AccountSelector';
 import { getStyles } from './styles';
 import { useUiContext } from '../../../../UIProvider';
+import SearchInput from '../../../../components/UI/SearchInput';
+import { useRoute } from '@react-navigation/native';
+
+export const allowedScreen = ['navigation/SCHEDULE_TAB', 'navigation/CLASSES_TAB', 'navigation/STUDENTS_TAB']
 
 interface ISheduleHeader {
   text?: string
   isSelection?: boolean;
-}
+};
+
 export const SheduleHeader: FC<ISheduleHeader> = memo(({ text, isSelection }) => {
   const { colors } = useUiContext();
+  const route = useRoute();
   const styles = useMemo(() => getStyles(colors), [colors]);
+  const [searchMode, setSearchMode] = useState(false);
+
+  const toggleSearchMOde = () => {
+    if (allowedScreen.includes(route.name)) {
+    setSearchMode(!searchMode);
+    };
+  };
 
   return (
     <View style={styles.container}>
-      {isSelection && <AccountSelector isTitle={!!text} />}
-      {text && <Text numberOfLines={1} style={styles.headerText}>{text}</Text>}
-      <View style={styles.search}>
-        <SearchIcon />
-      </View>
+      {
+        searchMode
+          ? <SearchInput editMode={toggleSearchMOde} />
+          : <>
+            {isSelection && <AccountSelector isTitle={!!text} />}
+            {text && <Text numberOfLines={1} style={styles.headerText}>{text}</Text>}
+            <TouchableOpacity onPress={toggleSearchMOde}>
+              <SearchIcon />
+            </TouchableOpacity>
+          </>
+      }
     </View>
   );
 });
